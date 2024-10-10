66c5ed0a6732a7cf0e944a34ff6e6b1d7005195b
TmsIbrr <tom.ibarra@duocuc.cl>
2024-10-10 16:05:45 -0300

    pagina chat y lista agregadas

---
+++ src/app/register/register.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+
+  email: string = "";
+  password: string = "";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  registrar(){
+    
+  }
+
+}
